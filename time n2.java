752a2b5ac9e2a156365038638aa80c0f3596f02a
kongumurali0-stack <kongumurali0@gmail.com>
2026-07-23 12:13:06 +0530

    Create time n2.java

---
+++ time n2.java
@@ -0,0 +1,12 @@
+package Day16;
+
+public class Tme_N2 {
+    static void main() {
+        for(int i=0;i<10;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
